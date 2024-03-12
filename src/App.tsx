4e155c2547b4b41f3fb474c0687b24f1b3ad0a97
yihao_dang <1335902682@qq.com>
2024-03-13 00:57:11 +0800

    'feat: 初始化文件拖拽'

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.less";
+import { useEffect, useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import "./App.less";
+const App = () => {
+  const hiddenInputRef = useRef<HTMLInputElement | null>(null);
+  const fileContainerRef = useRef<HTMLDivElement | null>(null);
 
+  useEffect(() => {
+    // 拖拽物品进入容器时触发
+    fileContainerRef.current?.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+    });
+    // 拖拽物品再容器上方时一直触发
+    fileContainerRef.current?.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    // 拖拽物品离开容器时触发
+    fileContainerRef.current?.addEventListener("dragleave", (e) => {
+      e.preventDefault();
+    });
+    // 拖拽物品进入容器候松开左键后触发
+    fileContainerRef.current?.addEventListener("drop", (e) => {
+      e.preventDefault();
+    });
+
+    // 监听隐藏文件选择框的change事件，可以支持正常的选择文件
+    hiddenInputRef.current?.addEventListener("change", (e) => {
+      const target = e.target as HTMLInputElement;
+      console.log(target.files);
+    });
+  }, []);
+
+  // 容器的点击事件，委托成隐藏的文件选择框的点击事件
+  const fileCOntainerClick = () => {
+    hiddenInputRef.current?.click();
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="file" ref={fileContainerRef} onClick={fileCOntainerClick}>
+      <div className="hidden-input" style={{ display: "none" }}>
+        <input ref={hiddenInputRef} type="file" multiple />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div className="file-icon"></div> */}
+      <div className="file-text">请选择文件上传或拖拽上传</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
